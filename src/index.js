eae65cb5a704a029ea159171c42dd81cf7539142
James Loewen <jamesfloewen@gmail.com>
2022-04-21 20:20:29 -0400

    Add inital functionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 // import './style.css';
 
+const toDoList = document.querySelector('#to-do-app');
 const addBtn = document.querySelector('#add-btn');
 const cardDiv = document.querySelector('.card');
 const titleInput = document.querySelector('input.title');
+const initialItemInput = document.querySelector('#initial-input');
+const arrStorage = [];
 
 titleInput.addEventListener('change', (e) => {
   console.log(e.target.value);
@@ -14,5 +17,33 @@ titleInput.addEventListener('focusout', () => console.log("HOCUS POCUS THERE'S P
 cardDiv.tabIndex = 0;
 
 const createEntry = () => {
+  const listItemDiv = document.createElement('div');
+  listItemDiv.classList.add('list-item');
+
+  const checkbox = document.createElement('input');
+  checkbox.type = "checkbox";
+
+  const textInput = document.createElement('input');
+  textInput.classList.add('input-item');
+  textInput.type = "text";
+
+  listItemDiv.appendChild(checkbox);
+  listItemDiv.appendChild(textInput);
+
+  cardDiv.appendChild(listItemDiv);
   
+  textInput.focus();
+  textInput.addEventListener('keyup', (e) => {
+    if (e.key === "Enter") {
+      arrStorage.push(e.target.value);
+      createEntry();
+    }
+  });
 }
+
+initialItemInput.addEventListener('keyup', (e) => {
+  if (e.key === "Enter") {
+    arrStorage.push(e.target.value);
+    createEntry();
+  }
+});
